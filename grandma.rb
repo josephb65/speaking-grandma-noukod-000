b07474c1175695ee8ca983d71ca5d98bb20202da
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-11 15:04:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
        elsif phrase ="Hi Nana, how are you?"
           return "HUH?! SPEAK UP, SONNY!"
-             elsif"
+             elsif "WHAT DID YOU EAT TODAY?"
+                 return ""
   end 
 end 
